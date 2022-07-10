a018e4a35050399f2dbee2d7ed1421a6dd69cc1b
Konrad Milewski <konradmilewski2000@interia.eu>
2022-07-10 14:55:25 +0200

    Zmiany

---
+++ src/ApplicationCore/Interfaces/IAppLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IAppLogger<T>
+    {
+        void LogInformation(string message, params object[] args);
+
+        void LogWarning(string message, params object[] args);
+    }
+}
